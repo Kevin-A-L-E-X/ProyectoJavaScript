93865f7b234655f66bd4fd2029744eecb74b4a76
Kevin-A-L-E-X <kevinmontecinos2007@cesanrafael.org>
2024-11-19 14:19:15 -0400

    1

---
+++ JavaScrit/main.js
@@ -0,0 +1,42 @@
+let mensaje="Hola Mundo";
+console.log(mensaje);
+let numerouno=2;
+let numerodos=4;
+let numerotres=10;
+let resultadosuma=(numerouno+numerodos+numerotres);
+console.log("la suma es:" ,resultadosuma);
+let resultadoresta=(numerouno-numerodos-numerotres);
+console.log("la resta es:" ,resultadoresta);
+let resultadomultipicacion=(numerouno*numerodos*numerotres);
+console.log("la multiplicacion es:" ,resultadomultipicacion);
+let resultadodivision=(numerouno/numerodos/numerotres);
+console.log("la multiplicacion es:" ,resultadodivision);
+function sumar(){
+    let numerouno=3;
+    let numerodos=4;
+    let numerotres=16;
+    let resultado=(numerouno+numerodos+numerotres);
+    alert("La suma es:" + resultado);
+}
+function restar(){
+    let numerouno=3;
+    let numerodos=4;
+    let numerotres=16;
+    let resultado=(numerouno-numerodos-numerotres);
+    alert("La resta es:" + resultado);
+}
+function multiplicar(){
+    let numerouno=3;
+    let numerodos=4;
+    let numerotres=16;
+    let resultado=(numerouno*numerodos*numerotres);
+    alert("La multiplicacion es:" + resultado);
+}
+function dividir(){
+    let numerouno=3;
+    let numerodos=4;
+    let numerotres=16;
+    let resultado=(numerouno/numerodos/numerotres);
+    alert("La division es:" + resultado);
+}
+
